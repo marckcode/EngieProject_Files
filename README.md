90416b0c61ab4b3b0ab64afa08c5695a18db1ddc
marckcode <marckcode@gmail.com>
2024-06-10 16:10:51 -0300

    PC complete

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
-## Engie Project
+# Engie Project
 
-- Persistency Curves
+<!-- - Persistency Curves
 - Box Plots
 - Violin Plots
 - Heatmaps
 - Contour Plots
-- Flow Profiles
+- Flow Profiles -->
+
+## Persistency Curves
+It is observed the amount of operations points where the power flow is positive (blue), meaning that power is being exported and negative (red), where power is being imported.
+
+### Parameters:
+    - dataset=df_pwf        # Input dataset  (necessary)            
+    - col='P(MW)'           # Name of column to plot (necessary)
+    - k = 0                 # Index of Interconnection (necessary)
+    - path=path             # Path to save plot (optional)
+    - ax_fontsize=13        # Size of axes (optional)
+
+<img src="imgs/PC_Fluxo_RSUL.png">
+
+
+## Box Plots
--- src/plotter.py
+++ src/plotter.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.graph_objects as go 
+from matplotlib.colors import TwoSlopeNorm
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -10,10 +12,22 @@ class CreatePlots:
     def __init__(self):
         pass
 
-    def persistency_curve(self, dataset, nome_elo=None, col=None, 
-                          frm=None, to=None, path=None, ax_fontsize=None):
+    def persistency_curve(self, dataset, col, k, path=None, ax_fontsize=None):
+        df_pwf_elos   = dataset['Nome Elo'].unique()
+
+        if k == 0:
+            frm = df_pwf_elos[k][-2:]
+            to = 'T'
+        elif k == 5:
+            frm = df_pwf_elos[k].split('_')[1]
+            to  = 'T'
+        else:
+            splt1 = df_pwf_elos[k].split('_')
+            splt2 = splt1[1].split('-')
+            frm = splt2[0]
+            to = splt2[1]
         
-        dataset = pd.DataFrame(dataset[dataset['Nome Elo'] == nome_elo][col])
+        dataset = pd.DataFrame(dataset[dataset['Nome Elo'] == df_pwf_elos[k]][col])
 
         fig, ax = plt.subplots(figsize=(12, 6))
 
@@ -47,42 +61,58 @@ class CreatePlots:
         plt.tight_layout()
 
         if path != None:
-            plt.savefig(f'{path}/PC_{nome_elo}')
+            plt.savefig(f'{path}/PC_{df_pwf_elos[k]}')
 
         plt.show()
 
-    def box_plots(self, dataset, col=None, path=None, ax_fontsize=None, scenario=None):
-        fig, ax = plt.subplots(1, 1, figsize=(15, 6))
-        ax = sns.boxplot(
-            data=dataset, 
-            x="Nome Elo", 
-            y=col, 
-            notch=True, 
-            showcaps=False,
-            flierprops={"marker": "x"}, 
-            hue=dataset['Nome Elo'],
-            palette=sns.color_palette("hls", 8)
-        )
+    def box_plots(self, dataset, col, split_flows=True, 
+                  path=None, ax_fontsize=None, scenario=None):
+        
+        if split_flows == False:
+            fig, ax = plt.subplots(1, 1, figsize=(15, 6))
+            ax = sns.boxplot(
+                data=dataset, 
+                x="Nome Elo", 
+                y=col, 
+                notch=True, 
+                showcaps=False,
+                flierprops={"marker": "x"}, 
+                hue=dataset['Nome Elo'],
+                palette=sns.color_palette("hls", 8)
+            )
 
-        if scenario != None:
-            ax.set_title(f'{scenario}')
+            if scenario != None:
+                ax.set_title(f'{scenario}')
 
-        ax.set_ylabel(f'{col}', weight='bold', fontsize=13)
-        ax.set_xlabel('Nome Elo', weight='bold', fontsize=13)
-        ax.grid(True, alpha=0.3)
-        ax.set_xticklabels(dataset['Nome Elo'].unique(), rotation=30)
+            ax.set_ylabel(f'{col}', weight='bold', fontsize=13)
+            ax.set_xlabel('Nome Elo', weight='bold', fontsize=13)
+            ax.grid(True, alpha=0.3)
+            ax.set_xticklabels(dataset['Nome Elo'].unique(), rotation=30)
 
-        ax.tick_params(axis='both', which='major', labelsize=ax_fontsize)
-        ax.tick_params(axis='both', which='minor', labelsize=ax_fontsize)
+            ax.tick_params(axis='both', which='major', labelsize=ax_fontsize)
+            ax.tick_params(axis='both', which='minor', labelsize=ax_fontsize)
 
-        plt.tight_layout()
+            plt.tight_layout()
 
-        if path != None:
-            plt.savefig(f'{path}/BoxPlot_{col}')
+            if path != None:
+                plt.savefig(f'{path}/BoxPlot_{col}')
 
-        plt.show()
+            plt.show()
+        else:
+            fig, ax = plt.subplots(2, 1, figsize=(15, 6))
+            ax = sns.boxplot(
+                data=dataset, 
+                x="Nome Elo", 
+                y=col, 
+                notch=True, 
+                showcaps=False,
+                flierprops={"marker": "x"}, 
+                hue=dataset['Nome Elo'],
+                palette=sns.color_palette("hls", 8)
+            )
 
-    def violin_plots(self, dataset, col=None, path=None, ax_fontsize=None, scenario=None):
+
+    def violin_plots(self, dataset, col, path=None, ax_fontsize=None, scenario=None):
         fig, ax = plt.subplots(1, 1, figsize=(15, 6))
         ax = sns.violinplot(
             data=dataset, 
@@ -109,7 +139,7 @@ class CreatePlots:
 
         plt.show()
     
-    def create_heatmap(self, dataset, nome_elo, col=None, frm=None, to=None, path=None, ax_fontsize=None):
+    def create_heatmap(self, dataset, nome_elo, col, frm=None, to=None, path=None, ax_fontsize=None):
         max_day = max(sorted(list(set(dataset['Dia']))))
         min_day = min(sorted(list(set(dataset['Dia']))))
         n_days = len(sorted(list(set(dataset['Dia']))))
@@ -153,10 +183,149 @@ class CreatePlots:
             plt.savefig(f'{path}/Heatmap_{frm}_{to}')
         plt.show()
 
+    def flow_profiles(self, dataset, col, path=None, scenario=None):
+        df_pwf_elos = dataset['Nome Elo'].unique()
+        fig = go.Figure(
+            data=[
+                go.Scatter(x=dataset.index, y=dataset[dataset['Nome Elo'] == df_pwf_elos[0]][col]/1000, name=df_pwf_elos[0], legend="legend"),
+                go.Scatter(x=dataset.index, y=dataset[dataset['Nome Elo'] == df_pwf_elos[1]][col]/1000, name=df_pwf_elos[1], legend="legend"),
+                go.Scatter(x=dataset.index, y=dataset[dataset['Nome Elo'] == df_pwf_elos[2]][col]/1000, name=df_pwf_elos[2], legend="legend"),
+                go.Scatter(x=dataset.index, y=dataset[dataset['Nome Elo'] == df_pwf_elos[3]][col]/1000, name=df_pwf_elos[3], legend="legend"),
+                go.Scatter(x=dataset.index, y=dataset[dataset['Nome Elo'] == df_pwf_elos[4]][col]/1000, name=df_pwf_elos[4], legend="legend"),
+                go.Scatter(x=dataset.index, y=dataset[dataset['Nome Elo'] == df_pwf_elos[5]][col]/1000, name=df_pwf_elos[5], legend="legend"),
+                go.Scatter(x=dataset.index, y=dataset[dataset['Nome Elo'] == df_pwf_elos[6]][col]/1000, name=df_pwf_elos[6], legend="legend"),
+                go.Scatter(x=dataset.index, y=dataset[dataset['Nome Elo'] == df_pwf_elos[7]][col]/1000, name=df_pwf_elos[7], legend="legend"),
+                go.Scatter(x=dataset.index, y=dataset[dataset['Nome Elo'] == df_pwf_elos[8]][col]/1000, name=df_pwf_elos[8], legend="legend"),
+                go.Scatter(x=dataset.index, y=dataset[dataset['Nome Elo'] == df_pwf_elos[9]][col]/1000, name=df_pwf_elos[9], legend="legend"),
+            ],   
+            layout=dict(
+                legend={
+                    "title": f"<b>{scenario}</b>",
+                    "xref": "container",
+                    "yref": "container",
+                    "x": 1.2,
+                    "y": 0.8,
+                    "orientation": "v",
+                },
+                
+            ),
+        )
 
-    def create_contourplot(self):
-        pass
+        fig.update_layout(legend=dict(font=dict(family="Courier", size=20, color="black")),
+                        legend_title=dict(font=dict(family="Courier", size=25, color="black")))
+        fig.update_traces(visible="legendonly")
+        fig.for_each_trace(lambda t: t.update(name='<b>'+ t.name +'</b>'))
 
-    def flow_profiles(self):
-        pass
+        fig.update_layout(
+            title_x=0.5,
+            autosize=False,
+            width=1800,
+            height=800,
+            font_family="Courier",
+            xaxis_title=dict(text='<b># Operation Points</b>', font=dict(size=20, color='black')),
+            yaxis_title=dict(text=f'<b>{col[0]}(GW)</b>', font=dict(size=20, color='black')),
+            font_color="black",
+            title=dict(text=f"<b>Flow Profiles</b>", font=dict(size=45), yref='paper')
+        )
 
+        fig.update_xaxes(ticks="outside", 
+                    tickwidth=2, 
+                    tickcolor='black', 
+                    ticklen=5,
+                    tickfont_family="Arial Black"
+                    )
+
+        fig.update_yaxes(ticks="outside", 
+                    tickwidth=2, 
+                    tickcolor='black', 
+                    ticklen=5, 
+                    tickfont_family="Arial Black"
+                    )
+        
+        if path != None:
+            fig.write_html("../results/plot_flow_profiles.html")
+            
+        fig.show()
+
+
+    def create_contourplot(self, dataset, k, col, path=None, ax_fontsize=12):
+        df_pwf_elos   = dataset['Nome Elo'].unique()
+
+        # ['EXP_NE', 'Fluxo_NE-SE', 'Fluxo_N-S', 'Fluxo_SUL-SECO', 'Fluxo_NE-N', 'Fluxo_RSUL', 'Elo_FOZ-IBIUNA', 'Elo_PVEL-ARARQ', 'Elo_CPVBTB-PVEL', 'Elo_XINGU-SE']
+        if k == 0:
+            frm = df_pwf_elos[k][-2:]
+            to = 'T'
+        elif k == 5:
+            frm = df_pwf_elos[k].split('_')[1]
+            to  = 'T'
+        else:
+            splt1 = df_pwf_elos[k].split('_')
+            splt2 = splt1[1].split('-')
+            frm = splt2[0]
+            to = splt2[1]
+
+        Dia           = np.array(dataset[dataset['Nome Elo'] == df_pwf_elos[0]]['Dia'])
+        Hora          = np.array(dataset[dataset['Nome Elo'] == df_pwf_elos[0]]['Hora'])
+        Fluxo_NE      = np.array(dataset[dataset['Nome Elo'] == df_pwf_elos[0]][col])
+        Fluxo_NESE    = np.array(dataset[dataset['Nome Elo'] == df_pwf_elos[1]][col])
+        Fluxo_NS      = np.array(dataset[dataset['Nome Elo'] == df_pwf_elos[2]][col])
+        Fluxo_SULSECO = np.array(dataset[dataset['Nome Elo'] == df_pwf_elos[3]][col])
+        Fluxo_NEN     = np.array(dataset[dataset['Nome Elo'] == df_pwf_elos[4]][col])
+        Fluxo_RSUL    = np.array(dataset[dataset['Nome Elo'] == df_pwf_elos[5]][col])
+        Elo_FOZIBIUNA = np.array(dataset[dataset['Nome Elo'] == df_pwf_elos[6]][col])
+        Elo_PVELARARQ = np.array(dataset[dataset['Nome Elo'] == df_pwf_elos[7]][col])
+        Elo_CPVBPVEL  = np.array(dataset[dataset['Nome Elo'] == df_pwf_elos[8]][col])
+        Elo_XINGUSE   = np.array(dataset[dataset['Nome Elo'] == df_pwf_elos[9]][col])
+
+        df_dict = {'Dia': Dia, 'Hora': Hora, df_pwf_elos[0]: Fluxo_NE, df_pwf_elos[1]: Fluxo_NESE, 
+                   df_pwf_elos[2]: Fluxo_NS, df_pwf_elos[3]: Fluxo_SULSECO, df_pwf_elos[4]: Fluxo_NEN,
+                   df_pwf_elos[5]: Fluxo_RSUL, df_pwf_elos[6]: Elo_FOZIBIUNA, df_pwf_elos[7]: Elo_PVELARARQ,
+                   df_pwf_elos[8]: Elo_CPVBPVEL, df_pwf_elos[9]: Elo_XINGUSE}
+        df_pwf_total = pd.DataFrame(df_dict)
+        df_pwf_hour = df_pwf_total[::2].reset_index().drop('index', axis=1)
+
+        dia = np.unique(df_pwf_hour['Dia'])
+        hora = np.unique(df_pwf_hour['Hora'])
+
+        z_flows = []
+        vals = []
+        flow_cols = list(df_pwf_hour.columns[2:])
+
+        for j in range(0, len(flow_cols)):
+            lst = []
+            for i in range(0, 673):
+                if i % 24 == 0 and i != 0:
+                    lst.append(vals)
+                    vals = []
+                
+                if i == 672:
+                    break
+                vals.append(df_pwf_hour[flow_cols[j]][i])
+            z_flows.append(lst)
+
+
+        fig, ax = plt.subplots(1, 1, figsize=(12, 6))
+
+        norm   = TwoSlopeNorm(0)
+        contour = ax.contourf(hora, dia[::-1], np.array(z_flows[k][::-1]), 20, 
+                            cmap='RdBu_r', 
+                            extend='max', 
+                            norm=norm)
+
+        cbar1 = plt.colorbar(contour, ax=ax)
+        cbar1.formatter.set_powerlimits((3, 3))
+        cbar1.formatter.set_useMathText(True)
+        cbar1.ax.set_ylabel(f'{col}', rotation=90, weight='bold', size=12)
+
+        ax.set_xlabel("Hour", weight='bold', fontsize=ax_fontsize)
+        ax.set_ylabel("Day", weight='bold', fontsize=ax_fontsize)
+        ax.set_yticks(np.arange(2, 30))
+        ax.set_xticks(np.arange(0, 24))
+        ax.set_xticklabels(hora, rotation=45)
+        ax.set_title(fr'Fluxo {frm}$\rightarrow${to}', weight='bold', fontsize=18)
+
+        plt.tight_layout()
+        if path != None:
+            plt.savefig(f'{path}/ContourPlot_{frm}_{to}')
+
+        plt.show()  
